2677df7e433be4c798024610e5fe8f8160ef91e7
mariahclaugesen <mariahl2@illinois.edu>
2019-04-23 14:58:13 -0500

    Made activity structure easier to work with, started on replicating functionality of previous version.

--- app/src/main/java/com/example/finalproject/MainActivity.java
+++ app/src/main/java/com/example/finalproject/MainActivity.java
@@ -1,178 +1,70 @@
 package com.example.finalproject;
 
-import android.app.AlertDialog;
-import android.content.DialogInterface;
 import android.content.Intent;
-import android.support.design.widget.FloatingActionButton;
+import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.design.widget.BottomNavigationView;
-import android.view.LayoutInflater;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
-import android.widget.EditText;
-import android.widget.ListAdapter;
 import android.widget.ListView;
-import android.widget.TextView;
-import android.widget.Toast;
-
-import android.widget.AdapterView.OnItemLongClickListener;
-
-import android.content.DialogInterface.OnClickListener;
 
 import java.util.ArrayList;
-import java.util.Arrays;
 
 public class MainActivity extends AppCompatActivity {
-    private TextView mTextMessage;
+    /** List of questions user is tracking. */
+    final private ArrayList<String> questionList = new ArrayList<>();
 
-
-    private BottomNavigationView.OnNavigationItemSelectedListener mOnNavigationItemSelectedListener
-            = new BottomNavigationView.OnNavigationItemSelectedListener() {
-
-        @Override
-        public boolean onNavigationItemSelected(@NonNull MenuItem item) {
-            switch (item.getItemId()) {
-                case R.id.navigation_home:
-                    mTextMessage.setText(R.string.title_home);
-                    return true;
-                case R.id.navigation_result:
-                    mTextMessage.setText(R.string.title_result);
-                    return true;
-                case R.id.navigation_notifications:
-                    mTextMessage.setText(R.string.title_notifications);
-                    return true;
-            }
-            return false;
-        }
-    };
-
-    TextView textInfo;
-    ArrayList<String> Example;
-    ArrayAdapter<String> adapter;
+    //create data object to keep track of answers
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
 
+        //set up navigation bar
+        final BottomNavigationView navigation = findViewById(R.id.navigation);
+        navigation.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem v) {
+                switch (v.getItemId()) {
+                    case R.id.navigation_answer:
+                        MainActivity.this.setContentView(R.layout.activity_main);
+                        return true;
+                    case R.id.navigation_results:
+                        Intent showResults = new Intent(MainActivity.this, Results.class);
+                        startActivity(showResults);
+                        return true;
+                    case R.id.navigation_about:
+                        MainActivity.this.setContentView(R.layout.content_about);
+                        return true;
+                }
+                return false;
+            }
+        });
 
-        String[] categories = {"Daily", "Study", "Mood"};
-        Example = new ArrayList<>(Arrays.asList(categories));
-        adapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, Example);
-        final ListView theListView = (ListView) findViewById(R.id.categories);
-
-        theListView.setAdapter(adapter);
-
-
-        theListView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+        //set up question list
+        final ListView question_list = findViewById(R.id.question_list);
+        ArrayAdapter<String> questionAdapter = new ArrayAdapter<>(this, R.layout.activity_main, R.id.question_list, questionList);
+        question_list.setAdapter(questionAdapter);
+        question_list.setOnItemClickListener(new AdapterView.OnItemClickListener() {
             @Override
             public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
-                int itemPosition = position;
-                String value = (String) theListView.getItemAtPosition(position);
-
-                switch (value) {
-                    case "Mood":
-                        Intent moodScreen = new Intent(MainActivity.this, Mood.class);
-                        MainActivity.this.startActivity(moodScreen);
-                        break;
-
-
-                }
-
+                answerQuestion((String) question_list.getItemAtPosition(position));
             }
         });
-
-        theListView.setOnItemLongClickListener(new OnItemLongClickListener() {
-            // setting onItemLongClickListener and passing the position to the function
-            @Override
-            public boolean onItemLongClick(AdapterView<?> arg0, View arg1,
-                                           int position, long arg3) {
-                removeItemFromList(position);
-
-                return true;
-            }
-        });
-
-
-        textInfo = (TextView) findViewById(R.id.info);
-
-        FloatingActionButton fab = findViewById(R.id.fab);
-
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                openDialog();
-            }
-        });
-
     }
 
-
-    private void openDialog(){
-        LayoutInflater inflater = LayoutInflater.from(MainActivity.this);
-        View subView = inflater.inflate(R.layout.add_dialog, null, false);
-        final EditText subEditText = (EditText)subView.findViewById(R.id.dialogEditText);
-
-
-        AlertDialog.Builder builder = new AlertDialog.Builder(this);
-        builder.setTitle("Adding a new category");
-        builder.setView(subView);
-        AlertDialog alertDialog = builder.create();
-
-        builder.setPositiveButton("OK", new DialogInterface.OnClickListener() {
-            @Override
-            public void onClick(DialogInterface dialog, int which) {
-                String newItem = subEditText.getText().toString().trim();
-                if (newItem.equals("")) {
-                    Toast.makeText(MainActivity.this, "Please input some texts!", Toast.LENGTH_SHORT).show();
-                } else {
-                    Example.add(newItem);
-
-                    Toast.makeText(MainActivity.this, newItem + " added", Toast.LENGTH_SHORT).show();
-                }
-            }
-        });
-
-        builder.setNegativeButton("Cancel", new DialogInterface.OnClickListener() {
-            @Override
-            public void onClick(DialogInterface dialog, int which) {
-                Toast.makeText(MainActivity.this, "Cancel", Toast.LENGTH_LONG).show();
-            }
-        });
-
-        builder.show();
-
-    }
-    // method to remove list item
-    protected void removeItemFromList(int position) {
-        final int deletePosition = position;
-
-        AlertDialog.Builder alert = new AlertDialog.Builder(
-                MainActivity.this);
-
-        alert.setTitle("Delete");
-        alert.setMessage("Do you want delete this item?");
-        alert.setPositiveButton("YES", new OnClickListener() {
-            @Override
-            public void onClick(DialogInterface dialog, int which) {
-
-                Example.remove(deletePosition);
-                adapter.notifyDataSetChanged();
-                adapter.notifyDataSetInvalidated();
-            }
-        });
-        alert.setNegativeButton("CANCEL", new OnClickListener() {
-            @Override
-            public void onClick(DialogInterface dialog, int which) {
-                dialog.dismiss();
-            }
-        });
-
-        alert.show();
-
+    /** Add an answer to a question.
+     *
+     * @param chosenQuestion the question being answered
+     */
+    protected void answerQuestion(final String chosenQuestion) {
+        /*
+        pull up the answers to this particular question
+        get the user's selection
+        */
     }
 }
---
+++ app/src/main/java/com/example/finalproject/Results.java
@@ -0,0 +1,12 @@
+package com.example.finalproject;
+
+import android.os.Bundle;
+import android.support.design.widget.BottomNavigationView;
+import android.support.v7.app.AppCompatActivity;
+import android.support.annotation.NonNull;
+import android.view.MenuItem;
+import android.widget.TextView;
+
+public class Results extends AppCompatActivity {
+
+}
