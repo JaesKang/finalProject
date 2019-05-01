7efe47ec452c2cb3dbfe46953eb96fc0a5318b68
JaesKang <jkang41@illinois.edu>
2019-05-01 01:58:24 -0500

    Final

---
+++ app/src/main/java/com/example/finalproject/editAnswer.java
@@ -0,0 +1,84 @@
+package com.example.finalproject;
+
+import android.support.annotation.Nullable;
+import android.support.design.widget.TextInputLayout;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.content.Intent;
+import android.widget.Toast;
+
+import android.content.SharedPreferences;
+
+
+import java.util.ArrayList;
+import java.util.*;
+
+
+public class editAnswer extends AppCompatActivity {
+
+
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        SharedPreferences.Editor editor = getSharedPreferences("PRES", MODE_PRIVATE).edit();
+
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.layout_set_answers);
+
+
+        //set up answer entry
+        ArrayList<String> answers = new ArrayList<>();
+        TextInputLayout answerEntry = findViewById(R.id.add_answer_entry);
+        Button addAnswer = findViewById(R.id.add_answer_button);
+        addAnswer.setOnClickListener(w -> {
+            String answer = answerEntry.getEditText().getText().toString();
+            if (answer != null) {
+                answers.add(answer);
+                answerEntry.getEditText().setText("");
+                Toast.makeText(getApplicationContext(), answer + " added", Toast.LENGTH_LONG).show();
+            }
+            if (answers.size() >= QuestionData.MAX_ANSWERS) {
+                addAnswer.setVisibility(View.INVISIBLE);
+            }
+        });
+        //set up answer removal
+        Button removeAnswers = findViewById(R.id.remove_answers);
+        removeAnswers.setOnClickListener(w -> {
+            answers.retainAll(new ArrayList<String>());
+            addAnswer.setVisibility(View.VISIBLE);
+            editor.remove("answers");
+
+            editor.remove("answer0");
+            editor.remove("answer1");
+            editor.remove("answer2");
+            editor.remove("answer3");
+            editor.remove("answer4");
+
+            editor.remove("name0");
+            editor.remove("name1");
+            editor.remove("name2");
+            editor.remove("name3");
+            editor.remove("name4");
+            editor.apply();
+            Toast.makeText(getApplicationContext(), "All answers deleted", Toast.LENGTH_LONG).show();
+        });
+
+
+        //go back to answering questions
+        Button setAnswers = findViewById(R.id.set_answers_button);
+        setAnswers.setOnClickListener(w -> {
+            Intent a = new Intent(editAnswer.this, MainActivity.class);
+            a.putStringArrayListExtra("answers", answers);
+            Set<String> set = new HashSet<String>();
+            set.addAll(answers);
+            editor.putStringSet("answers", set);
+            editor.apply();
+            startActivity(a);
+        });
+
+
+    }
+
+
+
+}
---
+++ app/src/main/java/com/example/finalproject/result.java
@@ -0,0 +1,184 @@
+package com.example.finalproject;
+
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.support.design.widget.BottomSheetBehavior;
+import android.support.design.widget.TextInputLayout;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.support.design.widget.BottomNavigationView;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Spinner;
+import android.widget.TableLayout;
+import android.widget.TableRow;
+import android.widget.TextView;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.content.Intent;
+import android.content.SharedPreferences;
+import org.apache.commons.math3.distribution.ChiSquaredDistribution;
+
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class result extends AppCompatActivity {
+
+
+
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.layout_results);
+
+
+        //set up navigation bar
+        final BottomNavigationView navigation = (BottomNavigationView) findViewById(R.id.navigation);
+        navigation.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                switch (item.getItemId()) {
+                    case R.id.navigation_answer:
+                        Intent a = new Intent(result.this, MainActivity.class);
+                        startActivity(a);
+                        break;
+                    case R.id.navigation_results:
+                        setContentView(R.layout.layout_results);
+                        break;
+
+                    case R.id.navigation_about:
+                        setContentView(R.layout.layout_about);
+                        break;
+                }
+                return false;
+            }
+
+        });
+
+        SharedPreferences prefs = getSharedPreferences("PRES", MODE_PRIVATE);
+
+        //answer table
+        TableRow[] frequencyRows = {
+                findViewById(R.id.row0), findViewById(R.id.row1), findViewById(R.id.row2),
+                findViewById(R.id.row3), findViewById(R.id.row4)
+        };
+        TextView answer0 = findViewById(R.id.answer0);
+        TextView answer1 = findViewById(R.id.answer1);
+        TextView answer2 = findViewById(R.id.answer2);
+        TextView answer3 = findViewById(R.id.answer3);
+        TextView answer4 = findViewById(R.id.answer4);
+
+        TextView freq0 = findViewById(R.id.frequency0);
+        TextView freq1 = findViewById(R.id.frequency1);
+        TextView freq2 = findViewById(R.id.frequency2);
+        TextView freq3 = findViewById(R.id.frequency3);
+        TextView freq4 = findViewById(R.id.frequency4);
+
+        TextView pValue = findViewById(R.id.p_value);
+
+        String restoredText = prefs.getString("text", null);
+
+
+        String name0 = "";
+        String name1 = "";
+        String name2 = "";
+        String name3 = "";
+        String name4 = "";
+
+        String chi = "";
+
+        int frequency0 = 0;
+        int frequency1 = 0;
+        int frequency2 = 0;
+        int frequency3 = 0;
+        int frequency4 = 0;
+        int size = 0;
+
+
+        if (restoredText != null) {
+            name0 = prefs.getString("name0", "??");
+            name1 = prefs.getString("name1", "??");
+            name2 = prefs.getString("name2", "??");
+            name3 = prefs.getString("name3", "??");
+            name4 = prefs.getString("name4", "??");
+            frequency0 = prefs.getInt("answer0", 0);
+            frequency1 = prefs.getInt("answer1", 0);
+            frequency2 = prefs.getInt("answer2", 0);
+            frequency3 = prefs.getInt("answer3", 0);
+            frequency4 = prefs.getInt("answer4", 0);
+            chi = prefs.getString("chi", "0");
+            size = prefs.getInt("possible", 0);
+
+        }
+        answer0.setText(name0);
+        answer1.setText(name1);
+        answer2.setText(name2);
+        answer3.setText(name3);
+        answer4.setText(name4);
+
+        freq0.setText(Integer.toString(frequency0));
+        freq1.setText(Integer.toString(frequency1));
+        freq2.setText(Integer.toString(frequency2));
+        freq3.setText(Integer.toString(frequency3));
+        freq4.setText(Integer.toString(frequency4));
+
+
+
+        //Chi-squared test
+
+        int totalCount = frequency0 + frequency1 + frequency2 + frequency3 + frequency4;
+
+        int expectedFrequency = totalCount / size;
+
+
+        ArrayList<Integer> countArray = new ArrayList<>();
+
+        double chiStat = 0;
+
+        if (size == 1) {
+            countArray.add(frequency0);
+        }
+        if (size == 2) {
+            countArray.add(frequency0);
+            countArray.add(frequency1);
+        }
+        if (size == 3) {
+            countArray.add(frequency0);
+            countArray.add(frequency1);
+            countArray.add(frequency2);
+        }
+        if (size == 4) {
+            countArray.add(frequency0);
+            countArray.add(frequency1);
+            countArray.add(frequency2);
+            countArray.add(frequency3);
+        }
+        if (size == 5) {
+            countArray.add(frequency0);
+            countArray.add(frequency1);
+            countArray.add(frequency2);
+            countArray.add(frequency3);
+            countArray.add(frequency4);
+        }
+        for (int i = 0; i < size; i++) {
+
+            chiStat += Math.pow(countArray.get(i) - expectedFrequency, 2) / expectedFrequency;
+        }
+
+
+
+        ChiSquaredDistribution distribution = new ChiSquaredDistribution(size - 1);
+        String pvalue = Double.toString(1 - distribution.cumulativeProbability(chiStat));
+        pValue.setText(pvalue);
+
+
+
+
+
+
+
+    }
+}
